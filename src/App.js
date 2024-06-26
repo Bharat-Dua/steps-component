7a11e9d941d4000ab681933a9ebeeaeb6440a8ad
Bharat Dua <bharatdua13@gmail.com>
2024-06-26 20:22:56 +0530

    show/hide steps component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const messages = [
 ];
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlerNext() {
     if (step < messages.length) {
@@ -24,32 +25,39 @@ function App() {
     }
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handlerPrevious}
-          // disabled={step === 1}
-        >
-          prev
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={handlerNext}
-          // disabled={step === messages.length}
-        >
-          next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handlerPrevious}
+              // disabled={step === 1}
+            >
+              prev
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handlerNext}
+              // disabled={step === messages.length}
+            >
+              next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
